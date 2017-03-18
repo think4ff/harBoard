2d3a82637c7bd47948e50b015577ccb66a3aab7b
think4ff <think4ff@naver.com>
2017-03-18 17:56:14 +0900

    1st for harBoard

---
+++ src/main/java/com/har/HarBoardApplication.java
@@ -0,0 +1,12 @@
+package com.har;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarBoardApplication.class, args);
+	}
+}
